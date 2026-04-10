8a893bb19fa93092c15749a07fb52c217357d591
dary1zhu <46930209+dary1zhu@users.noreply.github.com>
2026-04-10 21:28:30 +0930

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,13 +64,17 @@ gh_dl "${MODULE_TEMPLATE_DIR}/bin/arm/cmpr" "https://github.com/j-hc/cmpr/releas
 gh_dl "${MODULE_TEMPLATE_DIR}/bin/x86/cmpr" "https://github.com/j-hc/cmpr/releases/latest/download/cmpr-x86"
 gh_dl "${MODULE_TEMPLATE_DIR}/bin/x64/cmpr" "https://github.com/j-hc/cmpr/releases/latest/download/cmpr-x86_64"
 
+mkdir -p "$BIN_DIR"
+gh_dl "${BIN_DIR}/paccer.jar" "https://github.com/j-hc/revanced-magisk-module/raw/main/bin/paccer.jar"
+gh_dl "${BIN_DIR}/dexlib2.jar" "https://github.com/j-hc/revanced-magisk-module/raw/main/bin/dexlib2.jar"
+
 idx=0
 for table_name in $(toml_get_table_names); do
 	# 过滤掉 main 配置
 	if [ -z "$table_name" ] || [ "$table_name" = "main" ]; then continue; fi
 	
 	# 1. 【核心修复】清理上一轮循环的残留数据，防止变量交叉污染
-	unset app_args
+	unset patches_src patches_ver cli_src cli_ver
 	declare -A app_args
 
 	t=$(toml_get_table "$table_name")
